b47376b121e8621710a52c941cb6915ec4b070b4
andi-nugroho <102736954+andi-nugroho@users.noreply.github.com>
2026-01-04 20:12:16 +0700

    uncomment routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createProfile,login, createAssets, getProfile, getAssets, getAssetsById, getAssetsByIdCreator, getAssetsWithPayment, verifyAssetsPayment } from '../controller/user_controller.js';
+import { createProfile, login, getChatWithPayment, verifyChatPayment,getProfileByWalletAddress, getAssetsWithPayment, verifyAssetsPayment,createAssets, getProfile, getAssets, getAssetsById, getAssetsByIdCreator, updateProfile } from '../controller/user_controller.js';
 
 const router = express.Router();
 router.post('/creator/register', createProfile);
@@ -9,8 +9,13 @@ router.post('/creator/assets', createAssets);
 router.get('/creator/assets', getAssets);
 router.get('/creator/assets/:id', getAssetsById);
 router.get('/creator/assets-creator/:idCreator', getAssetsByIdCreator)
+router.put('/creator/register/:id', updateProfile);
 
 // payment x402
 router.get('/creator/assets/:id/purchase', getAssetsWithPayment);
 router.post('/creator/assets/:id/verify', verifyAssetsPayment);
+
+router.get('/creator/find-by-wallet/:walletAddress', getProfileByWalletAddress);
+
+
 export default router;
